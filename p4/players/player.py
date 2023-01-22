fbb10b849d2be0bbe5be8300325ad30763c64b62
itsRed-v2 <paul.charignon.v2@gmail.com>
2023-01-22 16:51:48 +0100

    déclaration de la méthode abstraite play()

--- p4/players/player.py
+++ p4/players/player.py
@@ -1,7 +1,13 @@
+from abc import ABC, abstractmethod
+
 from p4.utils.color import Color
 from p4.utils.token import Token
 
-class Player():
+class Player(ABC):
 	def __init__(self, token: Token, displayName):
 		self.token = token
 		self.displayName = displayName + Color.RESET
+
+	@abstractmethod
+	def play(self, board, playCB):
+		pass
